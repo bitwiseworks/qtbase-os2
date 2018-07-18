97c69cb5087b1f802af07bd4f69844d79ee7a3e2
Dmitriy Kuminov <coding@dmik.org>
2018-07-18 23:31:49 +0300

    corelib: Align qt_os2.h to Qt5.
    This also implies removing wrappers for Win and Gpi calls which we
    used in Qt4 to fight with the FPU CW reset. It will be done in a
    different way now (see #22 for more info).

--- src/corelib/global/qt_os2.h
+++ src/corelib/global/qt_os2.h
@@ -1,42 +1,40 @@
 /****************************************************************************
 **
-** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
-** All rights reserved.
-** Contact: Nokia Corporation (qt-info@nokia.com)
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
 **
-** Copyright (C) 2010 netlabs.org. OS/2 parts.
+** Copyright (C) 2018 bww bitwise works GmbH. OS/2 parts.
 **
 ** This file is part of the QtCore module of the Qt Toolkit.
 **
 ** $QT_BEGIN_LICENSE:LGPL$
-** Commercial Usage
-** Licensees holding valid Qt Commercial licenses may use this file in
-** accordance with the Qt Commercial License Agreement provided with the
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
 ** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and Nokia.
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
 **
 ** GNU Lesser General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 as published by the Free Software
-** Foundation and appearing in the file LICENSE.LGPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU Lesser General Public License version 2.1 requirements
-** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-** In addition, as a special exception, Nokia gives you certain additional
-** rights.  These rights are described in the Nokia Qt LGPL Exception
-** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
 **
 ** GNU General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3.0 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU General Public License version 3.0 requirements will be
-** met: http://www.gnu.org/copyleft/gpl.html.
+** General Public License version 2.0 or (at your option) the GNU General
+** Public license version 3 or any later version approved by the KDE Free
+** Qt Foundation. The licenses are as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL2 and LICENSE.GPL3
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-2.0.html and
+** https://www.gnu.org/licenses/gpl-3.0.html.
 **
-** If you have questions regarding the use of this file, please contact
-** Nokia at qt-info@nokia.com.
 ** $QT_END_LICENSE$
 **
 ****************************************************************************/
@@ -47,18 +45,15 @@
 #ifdef __EMX__
 #define OS2EMX_PLAIN_CHAR
 #endif
+
+// Request everything we might ever need from including <os2.h>
 #define INCL_BASE
 #define INCL_PM
 #include <os2.h>
 
-#if defined(__GNUC__) && defined(__INNOTEK_LIBC__)
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-// wrappers for each Win* and Gpi* call that restore the FPU Control Word
-#include <API_FPU_CW_Wrappers.h>
 
 // Innotek GCC lacks some API functions in its version of OS/2 Toolkit headers
 
@@ -68,25 +63,7 @@ extern "C" {
 
 LONG APIENTRY WinQueryClipRegion(HWND hwnd, HRGN hrgnClip);
 
-extern inline LONG APIENTRY __FPU_CW_WinQueryClipRegion (HWND hwnd, HRGN hrgnClip)
-{
-    unsigned int cw = __FPU_CW_Get();
-    LONG ret = WinQueryClipRegion (hwnd, hrgnClip);
-    __FPU_CW_Set(cw);
-    return ret;
-}
-#define WinQueryClipRegion __FPU_CW_WinQueryClipRegion
-
 BOOL APIENTRY WinSetClipRegion(HWND hwnd, HRGN hrgnClip);
-
-extern inline LONG APIENTRY __FPU_CW_WinSetClipRegion (HWND hwnd, HRGN hrgnClip)
-{
-    unsigned int cw = __FPU_CW_Get();
-    LONG ret = WinSetClipRegion (hwnd, hrgnClip);
-    __FPU_CW_Set(cw);
-    return ret;
-}
-#define WinSetClipRegion __FPU_CW_WinSetClipRegion
 
 #ifdef __cplusplus
 }
@@ -98,7 +75,5 @@ extern inline LONG APIENTRY __FPU_CW_WinSetClipRegion (HWND hwnd, HRGN hrgnClip)
 // signal delivery (e.g. SIGCHLD). Note that the macro cannot be used in
 // assignment expressions so call it with the whole assignment as an argument.
 #define qDosNI(expr) while((expr) == ERROR_INTERRUPT)
-
-#endif // defined(Q_CC_GNU) && defined(__INNOTEK_LIBC__)
 
 #endif // QT_OS2_H
